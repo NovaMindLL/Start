cdf4446c9dc0edc169e1ae473c78e80d64c2c6d1
NovaMindLL <shift.kirill@mail.ru>
2025-06-28 22:41:02 +0300

    Create Test.java

---
+++ RomanCalc/RomanCalc1/Test.java
@@ -0,0 +1,31 @@
+package RomanCalc1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Roman number from 1 to 3");
+        String num = scanner.nextLine().trim().toUpperCase();
+        int result = romanNumber(num);
+        if (result == -1) {
+            System.out.println("Enter a Roman correct number from 1 to 3");
+        } else
+            System.out.println("Roman number:" + result);
+        scanner.close();
+    }
+
+    public static int romanNumber(String roman) {
+        switch (roman) {
+            case ("I"):
+                return 1;
+            case ("II"):
+                return 2;
+            case ("III"):
+                return 3;
+
+            default:
+                return -1; // incorrect input
+        }
+    }
+}
