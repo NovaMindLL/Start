830c938896fbd9f376a2c14ef5f06913222fa329
NovaMindLL <shift.kirill@mail.ru>
2025-06-28 22:40:59 +0300

    Create Test.java

---
+++ practicaStatic/Test.java
@@ -0,0 +1,11 @@
+package practicaStatic;
+
+public class Test {
+    /* MyClass objMyClass = new MyClass(); */
+
+    public void main(String[] args) {
+        MyClass.mainStatic();
+        /* objMyClass.mainNonStatic(); */
+    }
+
+}
