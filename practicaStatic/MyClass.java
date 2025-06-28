65f6614dd2cf16ccf30a052b76bedf7a84197e28
NovaMindLL <shift.kirill@mail.ru>
2025-06-28 22:40:57 +0300

    Create MyClass.java

---
+++ practicaStatic/MyClass.java
@@ -0,0 +1,12 @@
+package practicaStatic;
+
+public class MyClass {
+
+    public static void mainStatic() {
+        System.out.println("Статический метод");
+    }
+
+    public void mainNonStatic() {
+        System.out.println("Не статический метод");
+    }
+}
