17edc313122d193acee99f7257b8e20d65269cc1
NovaMindLL <shift.kirill@mail.ru>
2025-06-29 22:24:46 +0300

    Create Main.java

---
+++ ZadacjiNaZikli/e/Main.java
@@ -0,0 +1,12 @@
+package ZadacjiNaZikli.e;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 512) {
+
+            System.out.print(a + " ");
+            a = a * 2;
+        }
+    }
+}
