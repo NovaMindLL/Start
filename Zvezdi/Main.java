16ce67087a734dc66039e6c44fd3554726020862
NovaMindLL <shift.kirill@mail.ru>
2025-06-29 22:24:48 +0300

    Create Main.java

---
+++ Zvezdi/Main.java
@@ -0,0 +1,10 @@
+package Zvezdi;
+
+public class Main {
+    public static void main(String[] args) {
+        String z = "*****";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(z);
+        }
+    }
+}
