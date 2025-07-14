891d3d5cb8104e7ad2978b2fb1b18efe8150ea70
NovaMindLL <shift.kirill@mail.ru>
2025-07-14 22:26:15 +0300

    Update Main.java

--- streamFlatMap/primitiveArray/Main.java
+++ streamFlatMap/primitiveArray/Main.java
@@ -1,5 +1,27 @@
 package streamFlatMap.primitiveArray;
 
-public class Main {
+import java.util.stream.Collector;
 
+public class Main {
+   public static void main(String[] args) {
+    List<Person> persons = Data.getPersons(); 
+     
+    List<Person> persons2 = persons.stream()
+    .map(p-> new Person(p.getFirstName(), "Ivanov", p.getAge() + 100))
+    .collect(Collector.toList());
+
+Optional  <Person> person = persons.stream()
+.filter(p-> p.getAge()>30)
+.findFirst();
+.findAny();//Ищет любого больше 30
+
+long count = person.stream()
+.filter(p->p.getAge()>30)
+.count(); //Количество лиц старше 30 
+
+persons.forEach(System.out::println);
+
+
+System.out.println(person.get());
+   }
 }
