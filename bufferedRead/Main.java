9d3647c1be6fab16df0f4cb17fc1954d4cbcce61
NovaMindLL <shift.kirill@mail.ru>
2025-07-29 21:13:47 +0300

    Create Main.java

---
+++ bufferedRead/Main.java
@@ -0,0 +1,26 @@
+package bufferedRead;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> lines = new ArrayList<>();
+        Path path = Paths.get("Learning.txt");
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                lines.add(line);
+        } catch (IOException egnored) {
+
+        }
+        System.out.println(lines);
+    }
+
+}
