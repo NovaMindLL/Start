d250f06ca8251a19cd2403cd49f50e3f57fbd003
NovaMindLL <shift.kirill@mail.ru>
2025-08-04 20:25:30 +0300

    Update Main.java

--- completableFuture/Main.java
+++ completableFuture/Main.java
@@ -1,6 +1,7 @@
 package completableFuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -17,7 +18,26 @@ public class Main {
          * String result = future.join();
          * System.out.println(result);
          */
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println("Hi"),
-                Executors.newCachedThreadPool());
+        /*
+         * CompletableFuture<Void> future = CompletableFuture.runAsync(() ->
+         * System.out.println("Hi"),
+         * Executors.newCachedThreadPool());
+         */
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+
+            try {
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Hi";
+        });
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
