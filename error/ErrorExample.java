e1dc68dcc889f334479012c9c627415dab1e9497
NovaMindLL <shift.kirill@mail.ru>
2025-08-17 18:19:42 +0300

    Update ErrorExample.java

--- error/ErrorExample.java
+++ error/ErrorExample.java
@@ -1,23 +1,38 @@
 package error;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ErrorExample {
     private ABC abc = new ABC();
 
     public void foo() {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream("");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
         // System.out.println(1 / 0);
         // int array[] = { 1, 2, 3 };
         // array[5] = 3;
 
-        try {
-            abc.show();
-            System.out.println(1 / 0);
+        // try {
+        // abc.show();
+        // System.out.println(1 / 0);
 
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        } catch (ArithmeticException ex) {
-            ex.printStackTrace();
-        }
+        // } catch (NullPointerException | ArithmeticException e) {
+        // e.printStackTrace();
 
         /*
          * if (abc == null) {
