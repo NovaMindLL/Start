f557258e1579bd6552f946aa5cb7d1f3fe5cc499
NovaMindLL <shift.kirill@mail.ru>
2025-09-01 22:19:10 +0300

    Update Main.java

--- Calc/Main.java
+++ Calc/Main.java
@@ -1,5 +1,11 @@
 package Calc;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+   public static void main(String[] args) {
+    System.out.println("Введите число операто число");
+    Scanner sc = new Scanner(System.in);
+    String 
+   }
 }
