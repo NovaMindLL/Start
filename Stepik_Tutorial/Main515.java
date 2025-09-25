7deef4c455e3d51c766e96d4f29280c62d7175b8
NovaMindLL <shift.kirill@mail.ru>
2025-09-25 22:24:37 +0300

    Update Main515.java

--- Stepik_Tutorial/Main515.java
+++ Stepik_Tutorial/Main515.java
@@ -6,15 +6,16 @@ public class Main515 {
     public static void main(String[] args) {
         int n = readInput();
         boolean result = false;
-        int sumL = 0;
-        int sumR = 0;
+
         int[] array = String.valueOf(n).chars().map(c -> c - '0').toArray();
 
         for (int i = 1; i < array.length; i++) {
+            int sumL = 0;
+            int sumR = 0;
             for (int j = 0; j < i; j++) {
                 sumL += array[j];
             }
-            for (int j = 1; j < array.length; j++) {
+            for (int j = i; j < array.length; j++) {
                 sumR += array[j];
             }
             if (sumL == sumR) {
