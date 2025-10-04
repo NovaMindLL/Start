deefe722f9a14c1415a7956a91db161df73a0d15
NovaMindLL <shift.kirill@mail.ru>
2025-10-04 19:48:29 +0300

    Update Main631.java

--- Stepik_Tutorial/Main631.java
+++ Stepik_Tutorial/Main631.java
@@ -1,5 +1,27 @@
 package Stepik_Tutorial;
 
-public class Main631 {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Main631 {
+    public static void main(String[] args) {
+        List<Integer> data = readInput();
+        List<Integer> result = deleteZeroValues(data);
+        System.out.println(new Gson().toJson(result));
+    }
+
+    public static List<Integer> deleteZeroValues(List<Integer> data) {
+        return data.stream()
+                .filter(x -> !x.equals(1))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return new Gson().fromJson(input, new TypeToken<List<Integer>>() {
+        }.getType());
+    }
 }
