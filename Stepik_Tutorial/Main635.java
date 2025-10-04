b61b3fa200c431a5e2de4c5a07517a39629dc25e
NovaMindLL <shift.kirill@mail.ru>
2025-10-04 22:07:06 +0300

    Create Main635.java

---
+++ Stepik_Tutorial/Main635.java
@@ -0,0 +1,22 @@
+package Stepik_Tutorial;
+
+import java.util.*;
+
+public class Main635 {
+    public static void main(String[] args) {
+        List<List<Integer>> grid = new ArrayList<>();
+
+        grid.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        grid.add(new ArrayList<>(Arrays.asList(0, 1, 0)));
+        grid.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        Integer result = null;
+        int rows = grid.size();
+        int colums = 0;
+        if(rows==)
+
+
+        System.out.println(grid.size());
+    }
+
+}
